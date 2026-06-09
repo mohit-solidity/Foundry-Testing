13987970ba2c9d76c56bf5be08ba1ccc0ae345ec
Mohit <mohit96046@gmail.com>
2026-06-09 23:44:54 +0530

    Changed UserName Test Edge Cases

--- test/Subscription.t.sol
+++ test/Subscription.t.sol
@@ -12,11 +12,13 @@ contract SubscriptionTest is Test{
     Subscription public sub;
     address owner = makeAddr("owner");
     address creator1 = makeAddr("creator1");
+    address creator2 = makeAddr("creator2");
 
     function setUp() public {
         vm.startPrank(owner);
         sub = new Subscription();
         sub.addCreator(creator1);
+        sub.addCreator(creator2);
         vm.stopPrank();
     }
     function test_Pause() public {
@@ -50,6 +52,30 @@ contract SubscriptionTest is Test{
         console.log("This Is For Name");
         console.log(exists);
         assertEq(_name, name);
+        //Another Creator Can't Take Same Name
+        vm.expectRevert("UserName Already Occupied");
+        vm.prank(creator2);
+        sub.setCreatorData(name);
+    }
+    //Set Creator Data That is Already Used By Another Creator, But Later Change'd By Previous Creator
+    function testFuzz_UsedName(string memory name1,string memory name2) public {
+        vm.assume(bytes(name1).length>0);
+        vm.assume(bytes(name2).length>0);
+        vm.prank(creator1);
+        sub.setCreatorData(name1);
+        vm.expectRevert("UserName Already Occupied");
+        vm.prank(creator2);
+        sub.setCreatorData(name1);
+        //Creator 1 Change His UserName
+        vm.prank(creator1);
+        sub.setCreatorData(name2);
+        //Creator 2 Takes 1st Creator's Previous UserName
+        vm.prank(creator2);
+        sub.setCreatorData(name1);
+        //Creator 2 Can't Take 1st Creator's Change'd UserName
+        vm.expectRevert("UserName Already Occupied");
+        vm.prank(creator2);
+        sub.setCreatorData(name2);
     }
     function testFuzz_addPlan(uint _planId,uint _price,uint _duration) public{
         vm.assume(_price>0);
